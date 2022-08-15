6e7432b53cc23a6ab82ab17785a2e976f765bdc1
Shiva1231234 <109754082+Shiva1231234@users.noreply.github.com>
2022-08-15 06:12:14 +0530

    Add files via upload

---
+++ i neuron assignment 5.c
@@ -0,0 +1,164 @@
+ASSIGNMENT = 5 (INEURON JOB READY BOOT CAMP)
+Question = 1
+/*
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+int i,n;
+printf("enter a number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+printf("my sirg\n");
+
+}
+*\
+Assignment = 5
+Question = 2
+/*
+#include <stdio.h>
+
+int main()
+{
+int i,n;
+printf("enter a number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+printf("%d\n",i);
+}
+*\
+Assignment =5
+Question =3
+/*
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+int i,n;
+printf("enter a number");
+scanf("%d",&n);
+for(i=n;i>=1;i--)
+{
+printf("%d\n",i);
+}
+
+}
+*\
+Assignment =5
+Question =4
+/*
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+int i,n;
+printf("enter a number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+printf("%d\n",2*i-1);
+}
+
+}
+*\
+Assignment =5
+Question =5
+/*
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+int i,n;
+printf("enter a number");
+scanf("%d",&n);
+for(i=n;i>=1;i--)
+{
+printf("%d\n",2*i-1);
+}
+
+}
+*\
+Assignment =5
+Question =6
+/*
+#include <stdio.h>
+
+int main()
+{
+int i,n;
+printf("enter a number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+printf("%d\n",2*i);
+}
+}
+*\
+
+
+Assignment =5
+Question =7
+/*
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+int i,n;
+printf("enter a number");
+scanf("%d",&n);
+for(i=n;i>=1;i--)
+{
+printf("%d\n",2*i);
+}
+
+}
+*\
+Assignment =5
+Question =8
+/*
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+int i,n;
+printf("enter a number");
+scanf("%d",&n);
+for(i=n;i>=1;i--)
+{
+printf("%d\n",i*i);
+}
+
+}
+*\
+Assignment =5
+Question =9
+/*
+
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+int i,n;
+printf("enter a number");
+scanf("%d",&n);
+for(i=n;i>=1;i--)
+{
+
+printf("%d\n",i*i*i);
+}
+
+}
+*\
+Assignment = 5
+Question =10
+/*
+#include <stdio.h>
+int main() {
+  int n, i;
+  printf("Enter an integer: ");
+  scanf("%d", &n);
+  for (i = 1; i <= 10; ++i) {
+    printf("%d * %d = %d \n", n, i, n * i);
+  }
+  return 0;
+}
